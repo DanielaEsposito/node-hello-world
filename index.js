dcce7e2f88c701f39747d72b4317b1aa576fc6f9
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-15 13:04:33 +0100

    add package.json

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-const students = [
-    { name: 'Alex', year: 2021, vote: 6 },
-    { name: 'Susan', year: 2021, vote: 4 },
-    { name: 'Jim', year: 2022, vote: 8 },
-    { name: 'Kate', year: 2020, vote: 3 },
-    { name: 'Anne', year: 2021, vote: 7 },
-  ]
-  
-  //recuperare il nome degli studenti anno 2021 voto superiore 6 o superiore 
-  
-   const searchedStudent = students.filter(student => student.year === 2021 && student.vote >= 6).map(student => student.name)
-   
-  
-  
-   
-   console.log(searchedStudent)
+
+console.log("Hello World");
